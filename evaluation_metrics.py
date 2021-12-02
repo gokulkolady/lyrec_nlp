568747837a6972f9117eadf66e02b23338a03747
Nicholas Ramirez <ramirezn@mit.edu>
2021-12-01 21:58:19 -0500

    Fixed evaluation bug

--- evaluation_metrics.py
+++ evaluation_metrics.py
@@ -3,18 +3,18 @@ import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
+import pickle
 
 def cluster_embeddings(data):
-    X = pd.read_csv('HealthClusteringData.csv')
-    k_means = KMeans(n_clusters= 10)
+    k_means = KMeans(n_clusters= 3)
     pca = PCA(2)
-    pca_df = pca.fit_transform(X)
+    pca_df = pca.fit_transform(data)
     
-    labels = k_means.fit_predict(X)
-    u_labels = np.unique(labels)
+    label = k_means.fit_predict(data)
+    u_labels = np.unique(label)
 
     for u in u_labels:
-        plt.scatter(pca_df[labels==u , 0] , pca_df[labels==u , 1] , labels = u)
+        plt.scatter(pca_df[label==u , 0] , pca_df[label==u , 1] , label = u)
     plt.legend()
     plt.show()
 
@@ -32,6 +32,18 @@ def average_cosine_distance(data):
 
     return total_distance/total_pairs
 
+
+
+if __name__ == "__main__":
+    with open('100_artist_tfidf_dataset.pkl', 'rb') as f:
+        data = pickle.load(f)
+        embeddings = []
+        for song_list in data:
+            embeddings.append(song_list[5])
+        cluster_embeddings(embeddings)
+        print (average_cosine_distance(embeddings))
+
+    
 
 
 
